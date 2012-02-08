844690f9e48b867f9a8270b451e4233926232571
Jeff Nixon <breeman@gmail.com>
2012-02-08 11:26:53 -0500

    Set up remotely

---
+++ controller/main.rb
@@ -0,0 +1,41 @@
+# Default url mappings are:
+# 
+# * a controller called Main is mapped on the root of the site: /
+# * a controller called Something is mapped on: /something
+# 
+# If you want to override this, add a line like this inside the class:
+#
+#  map '/otherurl'
+#
+# this will force the controller to be mounted on: /otherurl.
+class MainController < Controller
+  # the index action is called automatically when no other action is specified
+  def index
+    @title = 'Eduventures AMM 2012'
+  end
+
+  def agendas
+    @title = 'Agendas | Eduventures AMM 2012'
+  end
+
+  def sponsors
+    @title = 'Sponsors | Eduventures AMM 2012'
+  end
+
+  def hotel
+    @title = 'Hotel Information | Eduventures AMM 2012'
+  end
+
+  def visiting
+    @title = 'Visiting Boston | Eduventures AMM 2012'
+  end
+
+  # the string returned at the end of the function is used as the html body
+  # if there is no template for the action. if there is a template, the string
+  # is silently ignored
+  def notemplate
+    @title = 'Eduventures AMM 2012'
+    
+    return 'There is no \'notemplate.xhtml\' associated with this action.'
+  end
+end
