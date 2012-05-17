016d26e29871c62939759ccd8da9b3860fa5609e
Al Dellicicchi <adellicicchi@eduventures.com>
2012-05-17 15:45:55 -0400

    updated merge error

--- controller/main.rb
+++ controller/main.rb
@@ -12,27 +12,6 @@
 class MainController < Controller
   # the index action is called automatically when no other action is specified
   
-  
-
-   def presentations
-    if  request.post?
-        flash[:error] = "Post Detected"
-        password = request[:password]
-          if password == '@mm2012'
-          session[:logged_in] = true
-          session[:wrong_pw] = false
-          flash[:error] = "Password Accepted"
-            else
-            flash[:error] = "Password Incorrect"
-            session[:wrong_pw] = true
-            session[:logged_in] = false
-            end
-        else
-        flash[:error] = "No Post Detected"
-        session[:wrong_pw] = false
-    end
-  end
-
   def logout
   session[:logged_in] = false
   session[:wrong_pw] = false
@@ -67,32 +46,29 @@ class MainController < Controller
   def presentations
      @title = 'Presentation Materials | Eduventures AMM 2012'
       if  request.post?
-          flash[:message] = "Post Action Detected"
-          @password = request[:password]
-          if @password == 'ammpre'
-              session[:logged_in] = true
-              flash[:message] = "Password Accepted"
-          else
+          #flash[:error] = "Post Detected"
+          password = request[:password]
+            if password == '@mm2012'
+            session[:logged_in] = true
+            session[:wrong_pw] = false
+            flash[:error] = "Password Accepted"
+              else
               flash[:error] = "Password Incorrect"
+              session[:wrong_pw] = true
               session[:logged_in] = false
-          end
-          #redirect rs(:presentations)
-      else
-          flash[:message] = "No Post Action Detected"
+              end
+          else
+          #flash[:error] = "No Post Detected"
+          #session[:wrong_pw] = false
       end
   end
 
-  def preslogout
-      session[:logged_in] = false 
-      flash[:error] = "Logged Out"
-      redirect rs(:presentations)  
-  end
   # the string returned at the end of the function is used as the html body
   # if there is no template for the action. if there is a template, the string
   # is silently ignored
   def notemplate
     @title = 'Missing / Bad Page - Eduventures AMM 2012'
     
-    return 'There is no \'notemplate.xhtml\' associated with this action.'
+    return 'Page Missing: There is no \'notemplate.xhtml\' associated with this action.'
   end
 end
